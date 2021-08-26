8a37583a384d8f3d5dcab0416106b112d0e53f3e
Pierre Barbera <pierre.pbdas@gmail.com>
2021-08-26 15:38:23 +0200

    updated read_any_seqfile to also work from stdin

--- common.hpp
+++ common.hpp
@@ -1,20 +1,26 @@
 #include "genesis/genesis.hpp"
 
-genesis::sequence::SequenceSet read_any_seqfile( std::string const& file )
+genesis::sequence::SequenceSet read_any_seqfile( std::string const& file, bool const from_stdin=false )
 {
   genesis::sequence::SequenceSet out_set;
 
   try {
-    genesis::sequence::FastaReader().read( genesis::utils::from_file( file ), out_set );
+    genesis::sequence::FastaReader().read( from_stdin
+      ? genesis::utils::from_stream( std::cin )
+      : genesis::utils::from_file( file ), out_set );
   } catch( std::exception& e ) {
     auto reader = genesis::sequence::PhylipReader();
     try {
-      reader.read( genesis::utils::from_file( file ), out_set );
+      reader.read( from_stdin
+        ? genesis::utils::from_stream( std::cin )
+        : genesis::utils::from_file( file ), out_set );
     } catch( std::exception& e ) {
       out_set.clear();
       reader.mode( genesis::sequence::PhylipReader::Mode::kInterleaved );
       try {
-        reader.read( genesis::utils::from_file( file ), out_set );
+        reader.read( from_stdin
+          ? genesis::utils::from_stream( std::cin )
+          : genesis::utils::from_file( file ), out_set );
       } catch( std::exception& e ) {
         throw std::invalid_argument {
           "Cannot parse sequence file(s): Invalid file format? (only phylip and fasta allowed)"
